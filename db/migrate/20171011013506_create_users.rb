30bd2c9c09d2a9bf76b060c67a7185f490bd2cc4
Nguyen Viet Hung <nguyen.viet.hung@framgia.com>
2017-10-09 10:03:36 +0700

    created model user
    Make a basic User model

---
+++ db/migrate/20171011013506_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+    add_column :users, :password_digest, :string
+  end
+end
