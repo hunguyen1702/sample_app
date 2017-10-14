332b736f6d92bc46fea0ed0a3cc501a6a905398b
Nguyen Viet Hung <hunguyen1702@gmail.com>
2017-10-14 10:30:10 +0700

    Finish user CRUD actions

--- db/migrate/20171011013506_create_users.rb
+++ db/migrate/20171011013506_create_users.rb
@@ -10,5 +10,6 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     add_index :users, :email, unique: true
     add_column :users, :password_digest, :string
     add_column :users, :remember_digest, :string
+    add_column :users, :is_admin, :boolean, default: false
   end
 end
